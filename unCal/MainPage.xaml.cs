91633810089ffe4f4bca870ed1ebbf7c154df8b4
unkei <unkei@yahoo.co.jp>
2012-02-04 11:02:47 +0900

    first commit

---
+++ unCal/MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.IO.IsolatedStorage;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MediaColor = System.Windows.Media.Color;
+
+namespace unCal
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private Color getColor(string colorname)
+        {
+            MediaColor mc = (MediaColor)System.Windows.Application.Current.Resources[colorname];
+            return new Color { A = mc.A, R = mc.R, G = mc.G, B = mc.B };
+        }
+
+        enum HAlign {CENTER, LEFT, RIGHT};
+        const int TILE_WIDTH = 173;
+        const int TILE_HEIGHT = 173;
+        const int TILE_MARGIN = 3;
+        const int TILE_TEXT_ROW = 8;
+        const int TILE_TEXT_COL = 8;
+
+        // cx = 0..7, cy = 0..7
+        private void drawString(WriteableBitmap wb, int cx, int cy, string str, Color color, int fontsize = 14, HAlign halign = HAlign.LEFT, int xadj=0, int yadj=0)
+        {
+            TextBlock tb = new TextBlock();
+            tb.Text = str;
+            tb.FontSize = fontsize;
+            tb.Foreground = new SolidColorBrush(color);
+
+            int x = (TILE_WIDTH  - TILE_MARGIN * 2) / TILE_TEXT_COL * cx + TILE_MARGIN;
+            int y = (TILE_HEIGHT - TILE_MARGIN * 2) / TILE_TEXT_ROW * cy + TILE_MARGIN;
+            switch (halign)
+            {
+                case HAlign.CENTER:
+                    x = TILE_WIDTH / 2 - (int)tb.ActualWidth / 2;
+                    break;
+                case HAlign.RIGHT:
+                    x -= (int)tb.ActualWidth;
+                    break;
+                case HAlign.LEFT:
+                    break;
+            }
+            x += xadj;
+            y += yadj;
+            wb.Render(tb, new TranslateTransform() { X = x, Y = y });
+        }
+
+        private static string GetLocalizedWeekdayName(DayOfWeek weekday)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)weekday];
+        }
+
+        public void createCalendarImage(string filename) //(DateTime d)
+        {
+            WriteableBitmap wb = new WriteableBitmap(173,173);
+
+            // Draw background in PhoneAccentColor (default tile background color)
+            Color ac = getColor("PhoneAccentColor");
+            Path rect = new Path();
+            rect.Stroke = new SolidColorBrush(Colors.Transparent);
+            rect.Fill = new SolidColorBrush(ac);
+            rect.StrokeThickness = 0;
+            RectangleGeometry rectGeometry = new RectangleGeometry();
+            rectGeometry.Rect = new Rect(0, 0, 173, 173);
+            rect.Data = rectGeometry;
+            wb.Render(rect, null);
+
+            // 
+            Color fc = getColor("PhoneForegroundColor");
+            drawString(wb, 0, 0, DateTime.Now.ToString("y"), fc, 16, HAlign.CENTER);
+
+            for (int i = 0; i < 7; i++) // starting from Sunday (0 is Sunday)
+            {
+                drawString(wb, i+2, 1, CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[i], fc, 14, HAlign.RIGHT);
+            }
+
+            for (int y = 0; y < 5; y++)
+            {
+                drawString(wb, 0, y+2, Convert.ToString(y+35), fc, 14, HAlign.LEFT, 0, 2);
+                for (int x = 0; x < 7; x++)
+                {
+                    drawString(wb, x+2, y+2, Convert.ToString(y*7+x+1), fc, 17, HAlign.RIGHT, -1);
+                }
+            }
+
+
+            wb.Invalidate();
+
+            using (IsolatedStorageFile isto = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (IsolatedStorageFileStream isstr = isto.OpenFile(filename, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+                {
+                    wb.SaveJpeg(isstr, wb.PixelWidth, wb.PixelHeight, 0, 100);
+                }
+            }
+        }
+
+        private void pinButton_Click(object sender, EventArgs e)
+        {
+            ShellTile TileToFind = ShellTile.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains("DefaultTitle=unCal"));
+
+            createCalendarImage("Shared\\ShellContent\\unCal.jpg");
+
+            if (TileToFind == null)
+            {
+                StandardTileData tileData = new StandardTileData
+                {
+                    BackgroundImage = new Uri("isostore:/Shared/ShellContent/unCal.jpg", UriKind.Absolute),
+                    Title = "unCal - wk37",
+                };
+
+                ShellTile.Create(new Uri("/MainPage.xaml?DefaultTitle=unCal", UriKind.Relative), tileData);
+            }
+
+        }
+    }
+}
---
+++ unCal/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("unCal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("unCal")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0ff1f59-b9f3-43fe-b8a0-d4d2172076f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
