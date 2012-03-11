2706419b33670c93b91593ba2a67d83dfd5de104
unkei <unkei@yahoo.co.jp>
2012-03-11 20:41:13 +0900

    made top label and bottom application bar tranparent

---
+++ isolatedstorage.cmd
@@ -0,0 +1,3 @@
+rem "c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts de 5f9e4f35-0552-4b06-8d11-9824857e7097 . 
+
+"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" dir de 5f9e4f35-0552-4b06-8d11-9824857e7097
